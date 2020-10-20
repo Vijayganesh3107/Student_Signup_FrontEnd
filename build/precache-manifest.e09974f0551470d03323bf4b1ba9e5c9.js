b099817d0157de885d4513e7f99c3cf0c6aa9724
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2020-10-20 11:02:03 +0530

    Build Files

---
+++ build/precache-manifest.e09974f0551470d03323bf4b1ba9e5c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fe93448aa670ca4104955697b5bf0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf4f1758700c4637283",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cbfc645087609c09ca67",
+    "url": "/static/css/main.dfac7a5b.chunk.css"
+  },
+  {
+    "revision": "bcf4f1758700c4637283",
+    "url": "/static/js/2.27807bcf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.27807bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfc645087609c09ca67",
+    "url": "/static/js/main.641411d9.chunk.js"
+  },
+  {
+    "revision": "4a9ddd8e9ff06807d69e",
+    "url": "/static/js/runtime-main.7c7131cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
